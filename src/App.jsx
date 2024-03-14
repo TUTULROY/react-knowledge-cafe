294481a273019c31718f5a5b30e1e466f0857786
TUTULROY <tutulroyjoy@gmail.com>
2024-03-14 23:49:16 +0600

    import data and proptypes

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Header/Blogs/Blogs'
+import Bookmarks from './components/Header/Bookmarks/Bookmarks'
+
+function App() {
+ 
+
+  return (
+    <>
+    
+     <Header></Header>
+    <div className='md:flex'>
+    <Blogs></Blogs>
+    <Bookmarks></Bookmarks>
+    </div>
+     
+    </>
+  )
+}
+
+export default App
